1570bcfd3539f1bc8a68fa060d20efb91ff7e954
syed <user.email>
2022-07-14 13:53:34 +0100

    attempt1 -not working

--- sketch/circlePacking.ts
+++ sketch/circlePacking.ts
@@ -10,10 +10,27 @@ interface Position {
 function calculatePackedCircles(areaWidth: number, areaHeight: number): Circle[] {
     //TODO: you need to implement this function properly!  
     //These are just a couple of random circles, with no consideration yet for overlap.
-    return [
-        { pos: { x: 300, y: 300 }, radius: 100 },
-        { pos: { x: random(0, areaWidth), y: random(0, areaHeight) }, radius: 40 },
-    ];
+    let numCircles = 10
+    let arrayCircles: Circle[] = []
+    arrayCircles.push({pos: { x: random(0, areaWidth), y: random(0, areaHeight) }, radius: 40}) //push initial circle
+
+    for (let i = 0; i < numCircles - 1; i++) {        
+        let generatedCircle: Circle = {pos: { x: random(0, areaWidth), y: random(0, areaHeight) }, radius: 40};
+
+        for(let restOfCircles of arrayCircles) {            
+            if ( distance(restOfCircles.pos, generatedCircle.pos) > restOfCircles.radius + generatedCircle.radius ) {
+                arrayCircles.push(generatedCircle)      // no overlap => add to canvas
+                console.log( distance(restOfCircles.pos, generatedCircle.pos) > restOfCircles.radius + generatedCircle.radius )
+            }
+        }
+    }
+        
+
+    return arrayCircles;
+    // return [
+    //     { pos: { x: 300, y: 300 }, radius: 100 },
+    //     { pos: { x: random(0, areaWidth), y: random(0, areaHeight) }, radius: 40 },
+    // ];
 }
 
 /** Returns the distance between two given positions.
@@ -25,3 +42,6 @@ function distance(p1: Position, p2: Position): number {
     const hyp = Math.sqrt(x * x + y * y);
     return hyp;
 }
+
+/*
+*/
